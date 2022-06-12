765d7c546a4d4c8b2bd126726c3bb412e69a4f3d
Kelvin Desman <kelvindesman@Kelvins-iMac.local>
2022-06-12 17:39:49 +0700

    feat: add Pipeline enu

--- enum.ts
+++ enum.ts
@@ -2,3 +2,10 @@ export enum Source {
   "SHOPEE" = 0,
   "TOKOPEDIA" = 1,
 }
+
+export enum Pipeline {
+  ShopeeTrends = "WRITE_TRENDS_SHOPEE",
+  TokopediaTrends = "WRITE_TRENDS_TOKOPEDIA",
+  ShopeeProducts = "WRITE_PRODUCTS_SHOPEE",
+  TokopediaProducts = "WRITE_PRODUCTS_TOKOPEDIA"
+}
