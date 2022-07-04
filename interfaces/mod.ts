f8d3579328d65ce4c0d823e1b749983375378ebf
Kelvin Desman <kelvindesman@Kelvins-iMac.local>
2022-07-05 05:58:33 +0700

    chore: fix return type

--- interfaces/mod.ts
+++ interfaces/mod.ts
@@ -2,3 +2,4 @@ export * from "./trend_interface.ts";
 export * from "./product_interface.ts";
 export * from "./product_image_interface.ts";
 export * from "./product_snapshot_interface.ts";
+export * from "./octokit.ts";
